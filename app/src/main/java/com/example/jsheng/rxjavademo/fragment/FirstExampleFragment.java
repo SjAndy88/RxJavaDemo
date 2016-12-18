54954743dafd719532a864712d34dd3ccc6c8de5
shengjun <flztsj@gmail.com>
2016-12-18 12:47:58 +0800

    更新依赖库，使用v4 Fragment

--- app/src/main/java/com/example/jsheng/rxjavademo/fragment/FirstExampleFragment.java
+++ app/src/main/java/com/example/jsheng/rxjavademo/fragment/FirstExampleFragment.java
@@ -1,11 +1,11 @@
 package com.example.jsheng.rxjavademo.fragment;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
